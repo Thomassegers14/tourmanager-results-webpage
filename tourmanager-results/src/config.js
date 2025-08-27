b0931f20f234aa0197add82b9300826ff99561af
Thomassegers14 <segers.thomas@telenet.be>
2025-08-27 19:22:12 +0200

    add function to config js

--- tourmanager-results/src/config.js
+++ tourmanager-results/src/config.js
@@ -2,4 +2,12 @@
 export const EVENT_CONFIG = {
   event_id: "vuelta-a-espana",
   event_year: "2025"
-};
+}
+
+export const formatRiderName = function(fullName) {
+    const parts = fullName.trim().split(' ')
+    if (parts.length < 2) return fullName
+    const firstName = parts.pop()
+    const lastName = parts.join(' ').toLowerCase()
+    return lastName.replace(/(^|\s|-)(\p{L})/gu, (_, sep, letter) => sep + letter.toLocaleUpperCase())
+}
